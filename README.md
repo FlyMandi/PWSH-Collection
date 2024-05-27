7d92103f19270dc3f743347d7e5cb3dd1807748b
Federico <steidlmartinez@gmail.com>
2024-05-27 03:49:11 +0200

    added simple backup

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # PWSH-Collection
+
+[Simple script to compress & back-up a folder with winRAR](https://github.com/FlyMandi/PWSH-Collection/backup.ps1), it will take a source Folder as the -f flag and a destination Folder as the -t flag, copy & compress everything from the source folder and create an aptly named backup.
+
+Example usage: ```backup -f "C:\Important Files\" -t "D:\Backup Folder\"```
+
+//TODO: Automate rolling backups (delete old files)
+//TODO: config file for rar.exe path and/or backup folder
---
+++ backup.ps1
@@ -0,0 +1,17 @@
+# example usage: rarBackup -f "C:\Important Files\" -t "D:\Backup Folder\"
+# -f is to be understood as  "from" and -t as "to"
+Param(
+    [string]$f,
+    [string]$t
+)
+
+# path to your installed rar.exe (NOT WinRAR.exe), default:
+$rar = "C:\Program Files\WinRAR\Rar.exe"
+
+$date = Get-Date -format "yy-MM-dd"
+$leafName = Split-Path -Path $f -Leaf
+$archiveName = "$leafName`_backup-$date.rar"
+$archive_full = "$t\$archiveName"
+
+# run rar.exe "a -u" (update) "-y" (presume yes) "-m5" (highest compression) "-t" (test after finishing)
+&$rar a -u -y -m5 -t $archive_full $f

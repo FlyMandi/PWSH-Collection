1ee687fe5882e2f996c09b1123595b6f610a68ea
Federico <steidlmartinez@gmail.com>
2024-05-29 03:32:40 +0200

    add HKEY_LOCALMACHINE support

--- updateWR.ps1
+++ updateWR.ps1
@@ -13,6 +13,7 @@ If(-Not('' -eq $reg)){
     $app = $reg
     $pathSet = $true
     $keyPath = "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Uninstall\$reg"
+    $keyPath2 = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$reg"
 }
 
 try {
@@ -21,30 +22,44 @@ try {
         $app =(((winget show $pkg | FINDSTR "Found").Trim("Found ")) -replace('(\[.*)')).Trim()
         #search in registry from app name
         $keyPath = "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Uninstall\$app"
+        $keyPath2 = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$app"
     }
 
-    Write-Host "Trying to update the registry for '$app'."
-    Write-Host "'$keyPath'"
+    # test validity of paths
+    $pathValid = Test-Path $keyPath
+    $path2Valid = Test-Path $keyPath2
 
-    # old version is the one installed
-    $oldVersion = (Get-Item -LiteralPath $keyPath).GetValue($valueName)
+    Write-Host "Trying to update the registry for '$app'.`n"
+    Write-Host "'$keyPath':`nFound path: $pathValid`n`n'$keyPath2'`nFound path: $path2Valid`n`n"
+
+    if ($pathValid){
+        $truePath = $keyPath
+        $oldVersion = (Get-Item -LiteralPath $truePath).GetValue($valueName)
+    } ElseIf ($path2Valid){
+        $truePath = $keyPath2
+        $oldVersion = (Get-Item -LiteralPath $truePath).GetValue($valueName)
+    } Else {
+        throw "Can't find a valid registry path."
+    }
+
     # pull newest version number from winget
     $newVersion = (winget show $pkg | FINDSTR "Version").Trim("Version: ")
 
-    if ($null -eq $oldVersion){
+    # if keypath doesn't exist, it doesn't exist.
+    if (-Not ($pathValid) -and -Not($path2Valid)){
         throw "DisplayVersion registry key for $app does not exist."
     }
     ElseIf($oldVersion -eq $newVersion) {
         Write-Host "`nRegistry key DisplayVersion for $app is up-to-date."
     } 
     else {
-        Set-ItemProperty -Path $keyPath -Name $valueName -Value $newVersion
+        Set-ItemProperty -Path $truePath -Name $valueName -Value $newVersion
         # setVersion = the new, changed value
-        $setVersion = (Get-Item -LiteralPath $keyPath).GetValue($valueName)
+        $setVersion = (Get-Item -LiteralPath $truePath).GetValue($valueName)
         Write-Host "`n Updated registry key to reflect winget package newest version:
     Outdated: $oldVersion`n    Now: $setVersion"
     }
 } 
 catch {
-    "No valid package name provided or no valid registry folder found from package name."
+    "No valid package name provided and/or no valid registry key found in folder."
 }

6615bd8b254d888763ec8ec61482e0e42877f1ba
Federico <steidlmartinez@gmail.com>
2024-12-01 17:57:33 +0100

    fix playback by running mpvnet through cmd

--- scripts/Set-Alarm.ps1
+++ scripts/Set-Alarm.ps1
@@ -11,8 +11,11 @@ Param(
     [switch]$status = $false
 )
 
+#TODO: fix error handling for:
+    # - trying to -s or -c an alarm that's not scheduled
+
 if ($cancel -or $status){
-    if ($name = ""){
+    if ($name -eq ""){
         throw "No alarm name specified."
     }
 
@@ -38,10 +41,6 @@ elseIf($status){
 }
 else{
 
-#TODO: add error handling for trying to create an alarm that already exists, or deleting one that doesn't.
-
-#TODO: parse alarm name from user input
-
     if (-Not (Test-Path $path)){
         throw "Not a valid filepath."
     }
@@ -53,7 +52,6 @@ else{
         
         if ($timeString -match "^([0-1]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$"){
             $_seconds = $timeString.Substring(6,2)
-            $alarmTimeTminus = New-TimeSpan -Hours $_hours -Minutes $_minutes -Seconds $_seconds
             $alarmTime = Get-Date -Hour $_hours -Minute $_minutes -Second $_seconds
         }
         else { 
@@ -79,14 +77,16 @@ else{
 
     if (Get-Command mpvnet -errorAction SilentlyContinue){
         $params = '--really-quiet', '--title=ALARM', '--fs', '--keep-open=no', '--loop-file=inf' 
+        $command = "mpvnet $path $params & exit"
 
-#FIXME: mpvnet not executing properly
-        $action = New-ScheduledTaskAction -Execute "mpvnet" -Argument "$params $path"
+        $action = New-ScheduledTaskAction -Execute "cmd" -Argument "/k $command"
         $trigger = New-ScheduledTaskTrigger -Once -At $alarmTime
-        $settings = New-ScheduledTaskSettingsSet -WakeToRun
+        $trigger.EndBoundary = ($alarmTime).AddSeconds(10).ToString("s")
+        $settings = New-ScheduledTaskSettingsSet -WakeToRun -DeleteExpiredTaskAfter 00:00:01
         $task = New-ScheduledTask -Action $action -Trigger $trigger -Settings $settings
 
         Register-ScheduledTask -TaskName $name -InputObject $task
+
     }
     else{
         throw "mpvnet not installed."

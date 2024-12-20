5700d5488a8ecc9e14515fa1ebe6bd54200aa8c4
FlyMandi <steidlmartinez@gmail.com>
2024-12-20 13:45:23 -0300

    better behaviour for 1-page docs

--- scripts/cheatsheet.ps1
+++ scripts/cheatsheet.ps1
@@ -8,15 +8,16 @@ param(
 if (-Not([string]::IsNullOrEmpty($inputLang))){ $language = $inputLang }
 else { $language = (&curl -s "cht.sh/:list" | fzf) }
 
+$topicList = (&curl -s "cht.sh/$language/:list")
+
 if (-Not([string]::IsNullOrEmpty($inputTopic))){ $topic = $inputTopic }
-else { $topic = (&curl -s "cht.sh/$language/:list" | fzf) }
+elseIf([string]::IsNullOrEmpty($topicList)) { $topic = $null }
+else { $topic = ($topicList | fzf) }
 
 if ([string]::IsNullOrEmpty($topic)){
     if ($language -match "./") { throw "ERROR: language $language has no default info page." }
-    $query = curl -s "cht.sh/$language`?style=rrt"
+    $query = &curl -s "cht.sh/$language`?style=rrt"
 }
-else { $query = curl -s "cht.sh/$language/$topic`?style=rrt" }
-
-#TODO: fix assembly/, has no default page
+else { $query = &curl -s "cht.sh/$language/$topic`?style=rrt" }
 
 $query | less -R

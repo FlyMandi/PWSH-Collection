96f78e3cc047121e6358e4a5d4316379b6fda31e
FlyMandi <steidlmartinez@gmail.com>
2024-12-20 13:34:54 -0300

    add error handling

--- scripts/cheatsheet.ps1
+++ scripts/cheatsheet.ps1
@@ -1,6 +1,22 @@
-$language = (&curl -s "cht.sh/:list" | fzf)
+param(
+    [Parameter(Mandatory = $false, position = 0)]
+    $inputLang,
+    [Parameter(Mandatory = $false, position = 1)]
+    $inputTopic
+)
 
-$topic = (&curl -s "cht.sh/$language/:list" | fzf)
+if (-Not([string]::IsNullOrEmpty($inputLang))){ $language = $inputLang }
+else { $language = (&curl -s "cht.sh/:list" | fzf) }
 
-if ([string]::IsNullOrEmpty($topic)){ &curl -s "cht.sh/$language`?style=rrt" | less -R }
-else { &curl -s "cht.sh/$language/$topic`?style=rrt" | less -R }
+if (-Not([string]::IsNullOrEmpty($inputTopic))){ $topic = $inputTopic }
+else { $topic = (&curl -s "cht.sh/$language/:list" | fzf) }
+
+if ([string]::IsNullOrEmpty($topic)){
+    if ($language -match "./") { throw "ERROR: language $language has no default info page." }
+    $query = curl -s "cht.sh/$language`?style=rrt"
+}
+else { $query = curl -s "cht.sh/$language/$topic`?style=rrt" }
+
+#TODO: fix assembly/, has no default page
+
+$query | less -R

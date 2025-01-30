f81a5a8aded7d505cc5d71819234b8c1d3e2ff09
Federico <steidlmartinez@gmail.com>
2025-01-30 11:37:25 +0100

    add installation detection

--- scripts/PlayniteBackgroundChangerImageCleaner.ps1
+++ scripts/PlayniteBackgroundChangerImageCleaner.ps1
@@ -1,5 +1,21 @@
-$backgroundChangerImageFolder = Join-Path $env:USERPROFILE "\scoop\persist\playnite\ExtensionsData\3afdd02b-db6c-4b60-8faa-2971d6dfad2a\Images"
-$backgroundChangerJsonPath = Join-Path $env:USERPROFILE "\scoop\persist\playnite\ExtensionsData\3afdd02b-db6c-4b60-8faa-2971d6dfad2a\BackgroundChanger"
+$extensionPath = $null
+if(Test-Path (Join-Path $env:LOCALAPPDATA "Playnite\ExtensionsData\3afdd02b-db6c-4b60-8faa-2971d6dfad2a")){ 
+    $extensionPath = Join-Path $env:LOCALAPPDATA "\Playnite\" 
+}
+elseif(Test-Path(Join-Path $env:USERPROFILE "\scoop\persist\playnite\ExtensionsData\3afdd02b-db6c-4b60-8faa-2971d6dfad2a")){ 
+    $extensionPath = Join-Path $env:USERPROFILE "\scoop\persist\playnite"
+}
+
+if($null -eq $extensionPath){
+    Write-Host "ERROR: couldn't find a BackgroundChanger ExtensionsData folder in any of the typical Playnite install locations. Please provide the full path to " -NoNewLine
+    Write-Host "..\Playnite\ExtensionsData\3afdd02b-db6c-4b60-8faa-2971d6dfad2a"
+    Read-Host $extensionPath
+    if(-Not(Test-Path $extensionPath)){ throw: "ERROR: the path you provided is invalid." }
+}
+
+$backgroundChangerImageFolder = Join-Path $extensionPath "ExtensionsData\3afdd02b-db6c-4b60-8faa-2971d6dfad2a\Images"
+
+$backgroundChangerJsonPath = Join-Path $extensionPath "ExtensionsData\3afdd02b-db6c-4b60-8faa-2971d6dfad2a\BackgroundChanger"
 
 function Test-HasCorrespondingJson{
     param(

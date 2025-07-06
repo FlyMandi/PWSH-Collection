e82a6da213e7443884e03e32305e614e8ec5d3cf
Federico <steidlmartinez@gmail.com>
2025-07-06 19:00:30 +0200

    fix yay not needing sudo

--- modules/packages.psm1
+++ modules/packages.psm1
@@ -2,34 +2,68 @@
 #For windows, scoop > winget > anything else
 #For linux, apt > dpkg > anything else
 #probably $isWindows and then just have a lil hierarchy
-Function Get-FromPkgmgr{ 
-    Param(
+Function Get-FromPkgmgr
+{
+    Param
+    (
         $pkgmgr,
         $trgt,
         $override = $null
     )
 
-    if (-Not (Get-Command $trgt -ErrorAction SilentlyContinue)){ 
-        if(-Not ($null -eq $override)){ &$pkgmgr install $override }
-        else { &$pkgmgr install $trgt }
+    $installPrefix = ""
+    $installCmd = ""
+
+    switch($pkgmgr)
+    {
+        "pacman"
+        {
+            $installCmd = "-S"
+            $installPrefix = "sudo "
+        }
+        "yay"
+        {
+            $installCmd = "-S"
+        }
+        Default
+        {
+            $installCmd = "install"
+        }
+    }
+
+    $pkgmgr += $installPrefix
+
+    if (-Not (Get-Command $trgt -ErrorAction SilentlyContinue))
+    {
+        if(-Not ($null -eq $override))
+        {
+            &$pkgmgr $installCmd $override
+        }
+        else
+        {
+            &$pkgmgr $installCmd $trgt
+        }
     }
 }
 Export-ModuleMember -Function Get-FromPkgmgr
 
-Function Get-ScoopPackage{
-    Param(
-        $scoopTrgt
-    )
+Function Get-ScoopPackage
+{
+    Param($scoopTrgt)
+
     $scoopDir = Join-Path $env:USERPROFILE -ChildPath "\scoop\apps\"
 
-    if (-Not (Test-Path (Join-Path -Path $scoopDir -ChildPath $scoopTrgt))) { 
+    if (-Not (Test-Path (Join-Path -Path $scoopDir -ChildPath $scoopTrgt)))
+    {
         &scoop install $scoopTrgt
     }
 }
 Export-ModuleMember -Function Get-ScoopPackage
 
-function Get-GitLatestReleaseURI{
-    param(
+function Get-GitLatestReleaseURI
+{
+    param
+    (
         [Parameter(Position = 0, mandatory = $false)]
         $sourceRepo,
         [Parameter(Position = 1, mandatory = $false)]
@@ -38,11 +72,13 @@ function Get-GitLatestReleaseURI{
         [switch]$preRelease = $false
     )
 
-        if ($preRelease){
+        if ($preRelease)
+        {
             Write-Host "Installing latest $namePattern release package from $sourceRepo..."
             $sourceURI = ((Invoke-RestMethod -Method GET -Uri "https://api.github.com/repos/$sourceRepo/releases")[0].assets | Where-Object name -like $namePattern).browser_download_url
         }
-        else{
+        else
+        {
             Write-Host "Installing latest $namePattern pre-release package from $sourceRepo..."
             $sourceURI = ((Invoke-RestMethod -Method GET -Uri "https://api.github.com/repos/$sourceRepo/releases/latest").assets | Where-Object name -like $namePattern).browser_download_url
         }

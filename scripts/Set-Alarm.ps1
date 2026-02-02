bfb4c3b539cdef114986572c891fdf05c83f5ecd
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:44:09 +0100

    only actual playback left to do

--- scripts/Set-Alarm.ps1
+++ scripts/Set-Alarm.ps1
@@ -1,63 +1,95 @@
 Param(
-    [Parameter(Mandatory = $true, Position = 0)]
-    [string]$alarmTimeString = "",
-    [Parameter(Mandatory = $true, Position = 1)]
-    [string]$path
+    [Parameter(Mandatory = $false, Position = 0)]
+    [string]$name = "",
+    [Parameter(Mandatory = $false, Position = 1)]
+    [string]$timeString = "",
+    [Parameter(Mandatory = $false, Position = 2)]
+    [string]$path = "",
+    [Parameter(Mandatory = $false, Position = 3)]
+    [switch]$cancel = $false,
+    [Parameter(Mandatory = $false, Position = 4)]
+    [switch]$status = $false
 )
 
-#TODO: run this only if prompted by user to do so
-# Unregister-ScheduledTask -TaskName 'ALARM' -Confirm:$false
+if ($cancel -or $status){
+    if ($name = ""){
+        throw "No alarm name specified."
+    }
+
+    $formattedTime = (((Get-ScheduledTask -TaskName $name -Verbose | Select-Object *).Triggers).StartBoundary -replace ".*T" -replace "[+].*")
+}
+
+if ($cancel){
+    Unregister-ScheduledTask -TaskName $name -Confirm:$false
+
+    Write-Host "Alarm with name " -NoNewline
+    Write-Host $name -BackgroundColor DarkCyan -NoNewline
+    Write-Host " scheduled for " -NoNewline
+    Write-Host $formattedTime -NoNewline
+    Write-Host " cancelled." -ForegroundColor Red
+}
+elseIf($status){
+
+    Write-Host "Alarm with name " -NoNewline
+    Write-Host $name -BackgroundColor DarkCyan -NoNewline
+    Write-Host " going off at " -NoNewline
+    Write-Host $formattedTime -NoNewline
+    Write-Host " today."
+}
+else{
 
 #TODO: add error handling for trying to create an alarm that already exists, or deleting one that doesn't.
 
 #TODO: parse alarm name from user input
 
-if (-Not (Test-Path $path)){
-    throw "Not a valid filepath."
-}
-
-if ($alarmTimeString -match "^([0-1]?[0-9]|2[0-3]):[0-5][0-9]:?[0-5]?[0-9]?$") {
-
-    $_hours = $alarmTimeString.Substring(0,2)
-    $_minutes = $alarmTimeString.Substring(3,2)
-    
-    if ($alarmTimeString -match "^([0-1]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$"){
-        $_seconds = $alarmTimeString.Substring(6,2)
-        $alarmTimeTminus = New-TimeSpan -Hours $_hours -Minutes $_minutes -Seconds $_seconds
-        $alarmTime = Get-Date -Hour $_hours -Minute $_minutes -Second $_seconds
-    }
-    else { 
-        $alarmTimeTminus = New-TimeSpan -Hours $_hours -Minutes $_minutes  
-        $alarmTime = Get-Date -Hour $_hours -Minute $_minutes
+    if (-Not (Test-Path $path)){
+        throw "Not a valid filepath."
     }
 
-    if ((Get-Date) -gt $alarmTime) {
-        throw "Timestamp has already passed."
+    if ($timeString -match "^([0-1]?[0-9]|2[0-3]):[0-5][0-9]:?[0-5]?[0-9]?$") {
+
+        $_hours = $timeString.Substring(0,2)
+        $_minutes = $timeString.Substring(3,2)
+        
+        if ($timeString -match "^([0-1]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$"){
+            $_seconds = $timeString.Substring(6,2)
+            $alarmTimeTminus = New-TimeSpan -Hours $_hours -Minutes $_minutes -Seconds $_seconds
+            $alarmTime = Get-Date -Hour $_hours -Minute $_minutes -Second $_seconds
+        }
+        else { 
+            $alarmTimeTminus = New-TimeSpan -Hours $_hours -Minutes $_minutes  
+            $alarmTime = Get-Date -Hour $_hours -Minute $_minutes
+        }
+
+        if ((Get-Date) -gt $alarmTime) {
+            throw "Timestamp has already passed."
+        }
     }
-}
-else {
-    throw "Not a valid time format."
-}
+    else {
+        throw "Not a valid time format."
+    }
 
-$fileName = [System.IO.Path]::GetFileNameWithoutExtension($path)
+    $fileName = [System.IO.Path]::GetFileNameWithoutExtension($path)
 
-Write-Host "alarm set to go off at " -NoNewline
-Write-Host $alarmTime -NoNewline -BackgroundColor Green
-Write-Host ", queued to play " -NoNewline
-Write-Host $fileName -BackgroundColor Cyan -NoNewline
+    Write-Host "Alarm with name '$name' set to go off at " -NoNewline
+    Write-Host $alarmTime -NoNewline -BackgroundColor Green
+    Write-Host ", queued to play " -NoNewline
+    Write-Host $fileName -BackgroundColor Cyan -NoNewline
 
 
-if (Get-Command mpvnet -errorAction SilentlyContinue){
-    $params = '--really-quiet', '--title=ALARM', '--fs', '--keep-open=no', '--loop-file=inf' 
+    if (Get-Command mpvnet -errorAction SilentlyContinue){
+        $params = '--really-quiet', '--title=ALARM', '--fs', '--keep-open=no', '--loop-file=inf' 
 
 #FIXME: mpvnet not executing properly
-    $action = New-ScheduledTaskAction -Execute "mpvnet" -Argument "$params $path"
-    $trigger = New-ScheduledTaskTrigger -Once -At $alarmTime
-    $settings = New-ScheduledTaskSettingsSet -WakeToRun
-    $task = New-ScheduledTask -Action $action -Trigger $trigger -Settings $settings
+        $action = New-ScheduledTaskAction -Execute "mpvnet" -Argument "$params $path"
+        $trigger = New-ScheduledTaskTrigger -Once -At $alarmTime
+        $settings = New-ScheduledTaskSettingsSet -WakeToRun
+        $task = New-ScheduledTask -Action $action -Trigger $trigger -Settings $settings
 
-    Register-ScheduledTask -TaskName 'ALARM' -InputObject $task
-}
-else{
-    throw "mpvnet not installed."
+        Register-ScheduledTask -TaskName $name -InputObject $task
+    }
+    else{
+        throw "mpvnet not installed."
+    }
+
 }

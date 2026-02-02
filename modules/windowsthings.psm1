3ee6c05b8d42cbcc25f99bbc4fef06f0f9db37ed
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:44:10 +0100

    format

--- modules/windowsthings.psm1
+++ modules/windowsthings.psm1
@@ -1,13 +1,19 @@
-function Test-IsNotWinTerm{
+function Test-IsNotWinTerm
+{
     $process = Get-CimInstance -Query "SELECT * from Win32_Process WHERE name LIKE 'WindowsTerminal%'"
     return($null -eq $process)
 }
 Set-Alias tinwt Test-IsNotWinTerm
 Export-ModuleMember -Function Test-IsNotWinTerm -Alias tinwt
 
-function Get-NewMachinePath{
+function Get-NewMachinePath
+{
     $temp = $env:Path
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-    if (-Not($temp.Length) -eq ($env:Path.Length)){Write-Host "`nEnvironment variables updated!" -ForegroundColor Green}
+
+    if(-Not($temp.Length) -eq ($env:Path.Length))
+    {
+        Write-Host "`nEnvironment variables updated!" -ForegroundColor Green
+    }
 }
 Export-ModuleMember -Function Get-NewMachinePath

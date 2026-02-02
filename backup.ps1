f8c95733ecc5f54f1db9d2b861827af80de29ce0
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:44:08 +0100

    corrections

--- backup.ps1
+++ backup.ps1
@@ -4,15 +4,13 @@ Param(
     [string]$f = $pwd,
     [string]$t = "T:\AutoBackup_MDrive\"
 )
+
 # path error handling
 If ((-Not (Test-Path $f))) {
     Throw "The source directory $f does not exist, please provide a valid path."
 } ElseIf (-Not (Test-Path $t)){
     Throw "The destination directory $t does not exist, please provide a valid path."
 }
-
-# # path to your installed rar.exe (NOT WinRAR.exe), default:
-# $rar = "C:\Program Files\WinRAR\Rar.exe"
 
 # setting 7zip path
 $7zipPath = "$env:USERPROFILE\scoop\apps\7zip\24.06\7z.exe"
@@ -30,9 +28,7 @@ $archiveName = "$leafName`_backup-$date.7z"
 # making sure there's only 1 `\`
 $archive_full = Join-Path -Path $t -ChildPath $archiveName
 
-# # run rar.exe "a -u" (add/update) "-y" (presume yes) "-m5" (highest compression) "-t" (test after finishing)
-# &$rar a -u -y -m5 -t $archive_full $f
-
 # run 7zip
 &$7zipPath u -mx=9 $archive_full $f
+# test path
 &$7zipPath t $archive_full

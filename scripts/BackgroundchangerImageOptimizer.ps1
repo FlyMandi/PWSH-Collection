214413b4e961bd18bfbef10f8804715880df53c9
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:44:09 +0100

    add new .ps1 sketches

---
+++ scripts/BackgroundchangerImageOptimizer.ps1
@@ -0,0 +1,4 @@
+#TODO: list all images in the BackgroundChanger folders
+#TODO: iterate through that list and compress (jpg re-encoding as an option)
+#TODO: Automatic trigger on Library Update
+#TODO: publish as extension if compelling

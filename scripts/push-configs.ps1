cc7cf22de21fe51f17b50aaef6f284316896a4a9
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:44:09 +0100

    WIP: file change detection

--- scripts/push-configs.ps1
+++ scripts/push-configs.ps1
@@ -26,8 +26,8 @@ $scoopDir = Join-Path $env:USERPROFILE -ChildPath "\scoop\apps\"
 $WinVimpath = Join-Path -PATH $env:LOCALAPPDATA -ChildPath "\nvim\"
 $RepoVimpath = Join-Path -PATH $dotfiles -ChildPath "\nvim\"
 
-$WinGlazepath = Join-Path -PATH $env:USERPROFILE -ChildPath "\.glzr\glazewm\config.yaml"
-$RepoGlazepath = Join-Path -PATH $dotfiles -ChildPath "\glazewm\config.yaml"
+$WinGlazepath = Join-Path -PATH $env:USERPROFILE -ChildPath "\.glzr\glazewm\"
+$RepoGlazepath = Join-Path -PATH $dotfiles -ChildPath "\glazewm\"
 
 #FIXME: this .json isn't the only file to copy, it doesn't include:
 # keybinds
@@ -39,7 +39,7 @@ $RepoGlazepath = Join-Path -PATH $dotfiles -ChildPath "\glazewm\config.yaml"
 #$WinTermPreviewPath = Join-Path -PATH $env:LOCALAPPDATA -ChildPath "\Packages\Microsoft\Windows.TerminalPreview_8wekyb3d8bbwe\LocalState\settings.json"
 #$RepoTermpath = Join-Path -PATH $dotfiles -ChildPath "\wt\settings.json"
 
-$RepoPSpath = Join-Path -PATH $dotfiles -ChildPath "\PowerShell\Microsoft.PowerShell_profile.ps1"
+$RepoPSpath = Join-Path -PATH $dotfiles -ChildPath "\PowerShell\"
 
 #TODO: turn function arguments into references whereever possible
 
@@ -113,41 +113,34 @@ function Get-Binary {
 
         Write-Host "$command successfully installed and added to path!" -ForegroundColor Green
     }
-    #TODO: change this to elseIf(there's no new release available)
     else{
         Write-Host "$command already installed, continuing..."
     }
-    #else{
-    #TODO: remove all old files (folder and shortcut) and call function recursively
-    #
-    #    if (Test-Path $repoNameFolder){
-    #        Remove-Item "$repoNameFolder\.." -Recurse -Force
-    #   }
-    #} 
 }
 
-function Get-FileChange{
+function Push-ChangedFiles{
     param(
-        $file1,
-        $file2
+        $folder1,
+        $folder2
     )
-    #TODO:
-    #if file 1 identical to file 2
-        #return true
-    #else false
-}
 
-function Remove-OnFileChange{
-    param(
-        $inputPath,
-        $outputPath
-    )
-   
-   #TODO: for (every child item in file folder and its counterpart){
-#        if (Get-FileChange $file1 $file2){
-#            Remove-Item -PATH $outputPath -Recurse
-#        }
-    #}
+#FIXME: cannot bind DifferenceObject parameter because it is null
+
+    $missingFiles = Compare-Object -ReferenceObject (Get-ChildItem -Path $folder1 -File) -DifferenceObject (Get-ChildItem -Path $folder2 -File) | Where-Object { $_.SideIndicator -eq '<=' }
+    $filesToReplace = Compare-Object -ReferenceObject (Get-ChildItem -Path $folder1 -File) -DifferenceObject (Get-ChildItem -Path $folder2 -File) | Where-Object { $_.SideIndicator -eq '=>' }
+    
+    foreach ($file in $missingFiles) {
+        $fileName = $file.InputObject.BaseName
+	$fileNameWithExtension = $fileName + $file.InputObject.Extension
+        Write-Host "file name: $fileNameWithExtension"
+        Copy-Item -Path (Join-Path -PATH $folder1 -ChildPath $fileNameWithExtension) -Destination (Join-Path -PATH $folder2 -ChildPath $fileNameWithExtension)
+    } 
+
+#FIXME:
+    foreach($file in $filesToReplace){
+        Remove-Item "$($file.InputObject)"
+        Copy-Item -Path "$folder1\$($file.InputObject)" -Destination "$folder2\$($file.InputObject)"
+    }
 }
 
 function Push-Certain
@@ -163,15 +156,15 @@ function Push-Certain
         throw "Not a valid path to copy config from."
     }
 
-    if (Test-Path $outputPath){
-        Write-Host "`nExisting config found in $outputPath, updating..."
-        Remove-OnFileChange $inputPath $outputPath
+    if (-Not(Test-Path $outputPath) || (Get-ChildItem $outputPath | Measure-Object).count -eq 0){
+        Write-Host "`nNo existing config found in $outputPath, pushing..."
+	Copy-Item $inputPath $outputPath -Recurse
     }
     else{
-        Write-Host "`nNo existing config found in $outputPath, pushing..."
+        Write-Host "`nExisting config found in $outputPath, updating..."
+    	Push-ChangedFiles $inputPath $outputPath
     }
 
-    Copy-Item -PATH $inputPath -Destination $outputPath -Recurse
     Write-Host "Config push successful.`n" -ForegroundColor Green -NoNewline
 }
 
@@ -204,10 +197,10 @@ Get-Binary glsl_analyzer "nolanderc/glsl_analyzer" -namePattern "*x86_64-windows
 Get-Binary premake5 "premake/premake-core" -namePattern "*windows.zip" -preRelease
 Get-Binary fd "sharkdp/fd" -namePattern "*x86_64-pc-windows-msvc.zip" 
 
-#TODO: finish implementing file change detection
-#Push-Certain $RepoVimpath $WinVimpath
-#Push-Certain $RepoGlazepath $WinGlazepath
-#Push-Certain $RepoPSpath $PROFILE
+#FIXME: finish implementing file change detection
+Push-Certain $RepoVimpath $WinVimpath
+Push-Certain $RepoGlazepath $WinGlazepath
+Push-Certain $RepoPSpath "$PROFILE.."
 
 #FIXME: for some reason Windows Terminal doesn't want to play nice with the paths.
 #Push-Certain $RepoTermpath $WinTermpath

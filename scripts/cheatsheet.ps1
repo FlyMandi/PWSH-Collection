36786dcbc10f2a8fd4f0dfe12d9250a76ad31c5e
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:44:09 +0100

    better format

--- scripts/cheatsheet.ps1
+++ scripts/cheatsheet.ps1
@@ -5,19 +5,19 @@ param(
     $inputTopic
 )
 
-if (-Not([string]::IsNullOrEmpty($inputLang))){ $language = $inputLang }
-else { $language = (&curl -s "cht.sh/:list" | fzf) }
+if(-Not([string]::IsNullOrEmpty($inputLang))){ $language = $inputLang }
+else{ $language = (&curl -s "cht.sh/:list" | fzf) }
 
 $topicList = (&curl -s "cht.sh/$language/:list")
 
-if (-Not([string]::IsNullOrEmpty($inputTopic))){ $topic = $inputTopic }
+if(-Not([string]::IsNullOrEmpty($inputTopic))){ $topic = $inputTopic }
 elseIf([string]::IsNullOrEmpty($topicList)) { $topic = $null }
 else { $topic = ($topicList | fzf) }
 
-if ([string]::IsNullOrEmpty($topic)){
+if([string]::IsNullOrEmpty($topic)){
     if ($language -match "./") { throw "ERROR: language $language has no default info page." }
     $query = &curl -s "cht.sh/$language`?style=rrt"
 }
-else { $query = &curl -s "cht.sh/$language/$topic`?style=rrt" }
+else{ $query = &curl -s "cht.sh/$language/$topic`?style=rrt" }
 
 $query | less -R

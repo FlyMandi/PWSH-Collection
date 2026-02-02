9a45c53a8d7321f874527bd8c037f9def80c08b2
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:44:09 +0100

    add drive modularity

--- scripts/push-configs.ps1
+++ scripts/push-configs.ps1
@@ -1,4 +1,12 @@
-$RepoPath = "T:\Repository\dotfiles"
+if (Test-Path "T:\Repository\dotfiles\"){
+	$RepoPath = "T:\Repository\dotfiles\"
+}
+elseIf (Test-Path "C:\Repository\dotfiles\"){
+	$RepoPath = "C:\Repository\dotfiles\"
+}
+else{
+	throw "No valid Repository\dotfiles\ path found."
+}
 
 $WinVimpath = Join-Path -PATH $env:LOCALAPPDATA -ChildPath "\nvim\"
 $RepoVimpath = Join-Path -PATH $RepoPath -ChildPath "\nvim\"
@@ -7,9 +15,10 @@ $WinGlazepath = Join-Path -PATH $env:USERPROFILE -ChildPath "\.glzr\glazewm\conf
 $RepoGlazepath = Join-Path -PATH $RepoPath -ChildPath "\glazewm\config.yaml"
 
 $WinTermpath = Join-Path -PATH $env:LOCALAPPDATA -ChildPath "\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+$WinTermPreviewPath = Join-Path -PATH $env:LOCALAPPDATA -ChildPath "\Packages\Microsoft\Windows.TerminalPreview_8wekyb3d8bbwe\LocalState\settings.json"
 
 $RepoTermpath = Join-Path -PATH $RepoPath -ChildPath "\wt\settings.json"
-$RepoPSpath = Join-Path -PATH $RepoPath -ChildPath "PowerShell\Microsoft.PowerShell_profile.ps1"
+$RepoPSpath = Join-Path -PATH $RepoPath -ChildPath "\PowerShell\Microsoft.PowerShell_profile.ps1"
 
 
 function Push-Config
@@ -38,5 +47,16 @@ function Push-Config
 
 Push-Config $RepoVimpath $WinVimpath
 Push-Config $RepoGlazepath $WinGlazepath
-Push-Config $RepoTermpath $WinTermpath
-Push-Config $RepoPSpath $PROFILE
+if (Test-Path $WinTermpath){
+	Push-Config $RepoTermpath $WinTermpath
+}
+if (Test-Path $WinTermPreviewPath){
+	Push-Config $RepoTermpath $WinTermPreviewPath
+}
+
+if (Test-Path $PROFILE){
+    Push-Config $RepoPSpath $PROFILE 
+}
+elseIf(Test-Path "$env:USERPROFILE\Documents\WindowsPowershell\Microsoft.Powershell_profile.ps1"){
+    Push-Config $RepoPSpath "$env:USERPROFILE\Documents\WindowsPowershell\Microsoft.Powershell_profile.ps1"
+}

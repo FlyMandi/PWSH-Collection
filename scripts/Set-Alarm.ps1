57787dddc2204afc7fb8c91c391cbadb56a51d14
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:44:09 +0100

    add proper input parsing

--- scripts/Set-Alarm.ps1
+++ scripts/Set-Alarm.ps1
@@ -1,21 +1,40 @@
 Param(
     [Parameter(Mandatory = $true, Position = 0)]
-    [int]$alarmTime = -1,
+    [string]$alarmTimeString = "",
     [Parameter(Mandatory = $true, Position = 1)]
     [string]$path
 )
 
-#TODO: normalize alarmTime to seconds
+if ($alarmTimeString -match "^([0-1]?[0-9]|2[0-3]):[0-5][0-9]:?[0-5]?[0-9]?$") {
 
-#TODO: get chopped off path name (just filename)
+    $_hours = $alarmTimeString.Substring(0,2)
+    $_minutes = $alarmTimeString.Substring(3,2)
+    
+    if ($alarmTimeString -match "^([0-1]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$"){
+        $_seconds = $alarmTimeString.Substring(6,2)
+        $alarmTimeTminus = New-TimeSpan -Hours $_hours -Minutes $_minutes -Seconds $_seconds
+        $alarmTime = Get-Date -Hour $_hours -Minute $_minutes -Second $_seconds
+    }
+    else { 
+        $alarmTimeTminus = New-TimeSpan -Hours $_hours -Minutes $_minutes  
+        $alarmTime = Get-Date -Hour $_hours -Minute $_minutes
+    }
+
+    if ((Get-Date) -gt $alarmTime) {
+        throw "Timestamp has already passed."
+    }
+}
+else {
+    throw "Not a valid time format."
+}
+
 $fileName = [System.IO.Path]::GetFileNameWithoutExtension($path)
 
-#TODO: normalize alarmTime to readable time string
-$readableTime = "3 minutes 10 seconds"
+
+#TODO: actually schedule to play the video or audio file
 
 
-Write-Host "alarm set to go off in " -NoNewline
-Write-Host $readableTime -NoNewline -BackgroundColor Green
+Write-Host "alarm set to go off at " -NoNewline
+Write-Host $alarmTime -NoNewline -BackgroundColor Green
 Write-Host ", queued to play " -NoNewline
 Write-Host $fileName -BackgroundColor Cyan -NoNewline
-

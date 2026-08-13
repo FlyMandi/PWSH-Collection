0078c53b5dcecbe44f0abd0baa141cacea7a094d
badacronym <terribleacronym@gmail.com>
2026-08-13 10:28:09 +0200

    add sync-repositories on windows

---
+++ scripts/sync-repositories.ps1
@@ -0,0 +1,63 @@
+
+function update_all
+{
+    $folders | ForEach-Object -Parallel {
+        if(-Not(Test-Path $_) -or (Test-Path $_ -PathType Leaf))
+        {
+            echo "Already up to date."
+        }
+
+        cd $folder
+
+        if(-Not(Test-Path "./.git/") -And -Not(Test-Path "./git"))
+        {
+            echo "Already up to date."
+            return;
+        }
+
+        git fetch
+        git pull
+    }
+}
+
+$folders=@(
+"C:/repository/bash-collection/",
+"C:/repository/blazeterm/",
+"C:/repository/dotfiles/",
+"C:/repository/dropfetch/",
+"C:/repository/flushback/",
+"C:/repository/fontflow/",
+"C:/repository/gitfluss/",
+"C:/repository/imgsurf/",
+"C:/repository/islescape/",
+"C:/repository/marino/",
+"C:/repository/puddle/",
+"C:/repository/PWSH-Collection/",
+"C:/repository/river2D/",
+"C:/repository/river2D_mapedit/",
+"C:/repository/river3D/",
+"C:/repository/vertstream/",
+
+"C:/repository/gitfluss/vendor/puddle/",
+"C:/repository/imgsurf/vendor/puddle/",
+"C:/repository/blazeterm/vendor/fontflow/",
+"C:/repository/river2D/vendor/fontflow/",
+"C:/repository/river2D/vendor/imgsurf/",
+"C:/repository/river3D/vendor/imgsurf/",
+"C:/repository/river3D/vendor/vertstream/",
+"C:/repository/marino/vendor/river2D/",
+"C:/repository/marino/vendor/fontflow/",
+"C:/repository/islescape/vendor/river2D/",
+"C:/repository/river2D_mapedit/vendor/river2D/",
+
+"C:/repository/river2D/vendor/imgsurf/vendor/puddle/",
+"C:/repository/river3D/vendor/imgsurf/vendor/puddle/",
+"C:/repository/marino/vendor/river2D/vendor/imgsurf/",
+"C:/repository/islescape/vendor/river2D/vendor/imgsurf/",
+"C:/repository/river2D_mapedit/vendor/river2D/vendor/imgsurf/",
+
+"C:/repository/marino/vendor/river2D/vendor/imgsurf/vendor/puddle/",
+"C:/repository/islescape/vendor/river2D/vendor/imgsurf/vendor/puddle/",
+"C:/repository/river2D_mapedit/vendor/river2D/vendor/imgsurf/vendor/puddle/")
+
+update_all
